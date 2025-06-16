e8f88c75be8b759866676909c7a00213484863b0
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2025-06-16 16:52:38 +0530

    creat new springboot project for saloon service

---
+++ salonservice/src/main/java/com/saloon/salonservice/SalonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.saloon.salonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonserviceApplication.class, args);
+	}
+
+}
